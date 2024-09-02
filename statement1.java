68ff8828c1ae72191d6cc9442ce7a97b21a104c6
Bishal Kumar <vishalkumar7970751360@gmail.com>
2024-09-02 23:32:24 +0530

    java_project

---
+++ statement1.java
@@ -0,0 +1,93 @@
+//ASSIGNMENT OPERATOR
+
+/* public class operator{
+    public static void main(String[] args) {
+        int a=5,var=a;
+        System.out.println("var"+var);
+        var=var+a;
+        System.out.println("var:"+var);
+        var=var-a;
+        System.out.println("var:"+var);
+        var=var*a;
+        System.out.println("var:"+var);
+        var=var/a;
+        System.out.println("var:"+var);
+        var=var%a;
+        System.out.println("var"+var);
+    }
+} */
+
+//ARITHMTIC OPERATOR
+/* public class operator
+{
+    public static void main(String[] args)
+     {
+        int a=10,b=5;
+        System.out.println("sum is:"+(a+b));
+        System.out.println("sub is:"+(a-b));
+        System.out.println("multiply is:"+(a*b));
+        System.out.println("div is:"+(a/b));
+        System.out.println("modulus is:"+(a%b));
+    }
+} */
+
+//RELATIONAL OPERATOR
+/* public class operator{
+    public static void main(String[] args) {
+        int a=7,b=11;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+    }
+}
+ */
+//LOGICAL OPERATOR
+/* public class operator{
+    public static void main(String[] args) {
+        System.out.println((5<3)&&(8<5));
+        System.out.println((5>3)&&(8<5));
+        System.out.println((5<3)||(8>5));
+        System.out.println((5>3)||(8<5));
+        System.out.println((5<3)||(8<5));
+        System.out.println(!(5!=8));
+        System.out.println(!(5>3));
+    }
+} */
+
+//urinary operator.
+/* public class operator {
+    public static void main(String[] args) {
+        int a=12,b=12;
+        int r1,r2;
+        r1=++a;
+        r2=--b;
+        System.out.println("Result is:"+r1);
+        System.out.println("Result is:"+r2);
+    }
+    
+}
+ */
+
+//bitwise operator.
+/* public class operator{
+    public static void main(String[] args) {
+        int a=5,b=7;
+        System.out.println("a&b:"+(a&b));
+        System.out.println("a|b:"+(a|b));
+        System.out.println("a^b:"+(a^b));
+        System.out.println("~a:"+~(a));
+    }
+} */
+
+//ternary operator
+/* public class operator{
+    public static void main(String[] args) {
+        int num1=20,num2=10;
+        int res=(num1>num2)?(num1+num2):(num1-num2);
+        System.out.println("sum is:"+res);
+    } 
+}*/
+
